058872840ee9c064c9692688ded4d5899072131f
kartikeshwar156 <74772576+kartikeshwar156@users.noreply.github.com>
2021-07-16 06:11:37 +0530

    Add files via upload

---
+++ scripts/JET-GAME-AGAIN.js
@@ -0,0 +1,268 @@
+var myGamePiece;
+var myObstacles = [];
+var DD = 0;
+var UU = 0;
+var g=0;
+var i;
+var turn=0;
+var rem=1;
+
+
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "brown", 10, 120);
+    
+}
+
+var myGameArea = {
+    canvas : document.getElementById("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 370;
+        this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+
+    stop : function() {
+        clearInterval(this.interval);
+    }   
+}
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
+  }
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    this.buttons = function() {
+        if((this.y <= 210) && (this.y >= 16) && (this.speedY>0))
+        {
+            show=document.getElementById("moveDown");
+            show1=document.getElementById("moveUp");
+            show.style.display = "none";
+            show1.style.display = "none";
+        }
+        if((this.y <= 220) && (this.y >= 210))
+        {
+            show=document.getElementById("moveUp");
+            show.style.display = "";
+        }
+        if((this.y <= 210) && (this.y >= 16) && (this.speedY<0))
+        {
+            show=document.getElementById("moveDown");
+            show1=document.getElementById("moveUp");
+            show.style.display = "none";
+            show1.style.display = "none";
+        }
+        if((this.y <= 15) && (this.y >= 11))
+        {
+            show=document.getElementById("moveDown");
+            show.style.display = "";
+        }
+        
+    }
+
+    this.newPos = function() {
+        
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        
+        
+    }
+
+    this.newPos1 = function() {
+
+        if((this.y <= 230)&&(this.y >= 220))
+        {
+            this.speedY = -2;
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.speedY = 0;
+            DD = DD + 1;
+
+        }
+        else
+        {
+            this.speedY = +2;
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.speedY = 0;
+            UU = UU + 1;
+        }
+
+    }     
+
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) ||
+        (mytop > otherbottom) ||
+        (myright < otherleft) ||
+        (myleft > otherright)) {
+          crash = false;
+        }
+        return crash;
+      }
+}
+
+
+
+function updateGameArea() {
+
+    turn=turn + 1;
+    if(turn >= 25)
+    {
+        rem = turn % 25;
+    }
+    
+    if((DD != 0) && (rem == 0) && (myGamePiece.y <= 230)&&(myGamePiece.y >= 225))
+    {
+        myGamePiece.speedY = 2;
+    }
+
+    if((UU != 0) && (rem == 0) && (myGamePiece.y <= 15)&&(myGamePiece.y >= 10))
+    {
+        myGamePiece.speedY = -2;
+    }
+
+    var x, y;
+
+
+    
+
+    myGamePiece.newPos();
+
+    
+    
+    
+    //myGameArea.clear();
+    //myObstacle.update();
+    //myGamePiece.update();
+    //if (myGamePiece.crashWith(myObstacle)) {
+        //myGamePiece.speedX = 0;
+        
+        //myGameArea.clear();
+        //myObstacle.update();
+        //myGamePiece.newPos1();
+        //myGamePiece.update();
+
+      //} else {
+        //myGameArea.clear();
+        //myObstacle.x -= 1;
+        //myObstacle.update();
+        //myGamePiece.newPos();
+        //myGamePiece.update();
+      //}
+      
+      for (i = 0; i < myObstacles.length; i += 1) {
+          if (myGamePiece.crashWith(myObstacles[i])) {
+            myGamePiece.newPos1();
+            return;
+          } 
+      }
+
+
+    
+      myGameArea.clear();
+      myGameArea.frameNo += 1;
+      if (myGameArea.frameNo == 1 || everyinterval(150)) {
+        x = myGameArea.canvas.width;
+        minHeight = 100;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        gap2 = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+
+        if(turn==1)
+        {
+            a=0;
+        }
+        else
+        {
+            a=x+gap + gap2;
+
+        }
+
+        turn=turn+1;
+
+        myObstacles.push(new component(height  , 200, "green", a , 260));
+        myObstacles.push(new component(height, 10, "blue", x + gap, 0));
+
+        
+        //myObstacles.push(new component(height, 10, "green", x + gap, 0));
+        //myObstacles.push(new component(height  , 10, "green", x +gap +gap2 , 260));
+      }
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+    }
+
+    
+    myGamePiece.buttons();
+    myGamePiece.update();
+    
+
+  }
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
+}
+
+  function moveup() {
+    myGamePiece.speedY = -1; 
+}
+
+function movedown() {
+    myGamePiece.speedY = 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1; 
+}
+
+function moveright() {
+    myGamePiece.speedX += 1; 
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
+
+
+
+
+
+
+startGame();
+
